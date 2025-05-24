7d6c9073961eea3fe6dde20ee1bd63b064e6d54b
geoffreyFernando <fernandogeoffrey29@gmail.com>
2025-05-24 20:15:04 +0530

    feat : Added some code

--- force-app/main/default/lwc/membership_app/membership_app.js
+++ force-app/main/default/lwc/membership_app/membership_app.js
@@ -3,5 +3,10 @@ import { LightningElement, wire, track } from 'lwc';
 
 
 export default class Membership_app extends LightningElement {
+    @track selectedItem;
 
+    handleItemSelected(event){
+        console.log('from membership APP : ', event.detail.description);
+        this.selectedItem = event.detail.description
+    }
 }
